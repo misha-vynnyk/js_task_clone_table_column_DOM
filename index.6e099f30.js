4eac65e49c49496f6ddd90ef575fbf259e90a4a5
Misha Vynnyk <vinnikmisha94@gmail.com>
2024-07-06 09:42:34 +0200

    make build

---
+++ index.6e099f30.js
@@ -0,0 +1,2 @@
+for(var e=document.querySelector("table"),r=0;r<e.rows.length;r++){var l=e.rows[r].cells[1],n=document.createElement("td"),o=e.rows[r].cells[e.rows[r].cells.length-1];n.innerHTML=l.innerHTML,e.rows[r].insertBefore(n,o)}
+//# sourceMappingURL=index.6e099f30.js.map
